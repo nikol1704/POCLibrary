3a7d893d035ea6f2673705270c254b53e0b4b282
nikol.1704 <nikol.lazarte@540deg.com>
2025-03-10 11:37:35 +0100

    config platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "POCLibrary",
+    platforms: [
+        .iOS(.v16) // Ahora estamos en 18 pero se considera 2-3 por debajo
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
